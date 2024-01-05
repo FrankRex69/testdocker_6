a7c99ae87ab2e4dc1a50472a0c2035ad80916850
Kj£99shjKu34qq <frare@publicisgroupe.net>
2024-01-06 00:59:11 +0100

    feat: update index.js with ??

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(port_app);
 console.log(`http://localhost:${process.env.PORT_HOST}`);
 
 app.get('/', function (req, res) {  
-  res.send('Hello World testDocker !!!')
+  res.send('Hello World testDocker !!! ??')
 })
 
 app.listen(port_app)
